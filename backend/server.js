4639bb9ee35c41c01c45d4a44a27473ccb9ad8d4
touchja7-lang <touchja7@gmail.com>
2026-02-12 00:07:04 +0700

    update-server

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use('/api/categories', require('./routes/category'));
 app.use('/api/news', require('./routes/news'));
 app.use('/api/comments', require('./routes/comment'));
 
-app.options('*', cors());
+app.options('(.*)', cors());
 
 // Health check
 app.get('/api/health', (req, res) => {
